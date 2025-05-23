8f618bf23f567d7be373b58e037785455e17294e
Sam Zipper <samzipper@ku.edu>
2025-05-23 10:26:48 -0500

    Finalize WaterUse dataprep

--- code/DataPrep_WaterUse.R
+++ code/DataPrep_WaterUse.R
@@ -5,13 +5,13 @@
 # Purpose: This script processes annual water use data from WIMAS to create three 
 #   output CSV files.
 #    County CSV file (applied irrigation by crop): WaterUseData_County.csv
-#    | Year | FIPS | Crop | WaterUse_m3 | CropArea_ha
+#    | Year | FIPS | Crop | irrigation_mm_mean | irrigation_mm_median | irrigation_mm_p5| … | n_pdiv
 #    
 #    EKSRB CSV file (total water use by sector/source): WaterUseData_EKSRB.csv
-#    | Year | WaterUse_Total_m3 | WaterUse_Irrigation_m3 | {other sectors} | PercentGW_Total | PercentGW_Irrigation | {other sectors}
+#    | Year | Sector | waterUse_m3 | GWuse_m3 | SWuse_m3 |
 #    
 #    Alluvial Corridor CSV file (total water use by sector and source): WaterUseData_AlluvialCorridor.csv
-#    | Year | WaterUse_Total_m3 | WaterUse_Irrigation_m3 | {other sectors} | PercentGW_Total | PercentGW_Irrigation | {other sectors} | PercentGW_Total | PercentGW_Irrigation | {other sectors}
+#    | Year | Sector | waterUse_m3 | GWuse_m3 | SWuse_m3 |
 #
 # Subdomain boundaries are created in `DataPrep_Boundaries.R` script and saved in `data` folder.
 # 
@@ -51,11 +51,44 @@ ggplot() +
   geom_sf(data = sf_watershed) +
   geom_sf(data = sf_wimas_in, color = col.cat.red)
 
+
+# exclude WTR (enormous) and categories representing <1% of total use
+# sum total water use and water use by sector for each year
+df_wimas_in_total <- 
+  df_wimas_in |> 
+  subset(use != "WTR") |> 
+  group_by(year) |> 
+  summarise(
+    WaterUse_Total_m3 = sum(amt * 1233.4818375475, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+df_wimas_in_sector <- 
+  df_wimas_in |> 
+  subset(use != "WTR") |> 
+  group_by(year, use) |> 
+  summarise(
+    WaterUse_Sector_m3 = sum(amt * 1233.4818375475, na.rm = TRUE),
+    .groups = "drop"
+  ) |> 
+  left_join(df_wimas_in_total, by = "year") |> 
+  mutate(WaterUse_Sector_prc = WaterUse_Sector_m3 / WaterUse_Total_m3)
+
+ggplot(df_wimas_in_sector, aes(x = WaterUse_Sector_prc*100)) +
+  geom_histogram() +
+  facet_wrap(~use, scales = "free_x")
+
+# exclude uses
+use_to_exclude <- c("WTR", "AR", "CON", "DEW", "DOM", "FPR", "SED", "STK", "THX")
+
+# make trimmed version of df_wimas_in
+sf_wimas_trim <- subset(sf_wimas_in, !use %in% use_to_exclude)
+
 # Process for watershed ---------------------------------------------------
 
 # subset sf_wimas to any wells within the watershed
 sf_wimas_watershed <- 
-  st_filter(sf_wimas_in, sf_watershed)
+  st_filter(sf_wimas_trim, sf_watershed)
 
 # plot and inspect
 ggplot() +
@@ -64,9 +97,201 @@ ggplot() +
 
 # calculate total water use by year, use, and source
 df_wimas_watershedTotal <- 
-  sf_wimas_watershed %>%
-  group_by(year, use, source) %>%
+  sf_wimas_watershed |> 
+  st_drop_geometry() |> 
+  group_by(year, use, source) |> 
   summarise(
     WaterUse_Total_m3 = sum(amt * 1233.4818375475, na.rm = TRUE),
     .groups = "drop"
   )
+
+# plot to inspect
+ggplot(df_wimas_watershedTotal, aes(x = year, y = WaterUse_Total_m3, fill = source)) +
+  geom_col(position = "stack") +
+  facet_wrap(~ use, scales = "free_y")
+
+# calculate total water use in each year by sector
+df_wimas_watershedSector <- 
+  df_wimas_watershedTotal |> 
+  group_by(year, use) |> 
+  summarise(
+    waterUse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# calculate groundwater use in each year by sector
+df_wimas_watershedSectorGW <- 
+  df_wimas_watershedTotal |> 
+  subset(source == "G") |>
+  group_by(year, use) |> 
+  summarise(
+    GWuse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+
+# calculate surface water use in each year by sector
+df_wimas_watershedSectorSW <- 
+  df_wimas_watershedTotal |> 
+  subset(source == "S") |>
+  group_by(year, use) |> 
+  summarise(
+    SWuse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# join
+df_wimas_watershed_out <-
+  left_join(df_wimas_watershedSector, df_wimas_watershedSectorGW, by = c("year", "use")) |> 
+  left_join(df_wimas_watershedSectorSW, by = c("year", "use")) |>
+  rename(all_of(c("Year" = "year",
+                  "Sector" = "use")))
+
+write_csv(df_wimas_watershed_out, file.path("data", "WaterUseData_EKSRB.csv"))
+
+# Process for corridor ---------------------------------------------------
+
+# subset sf_wimas to any wells within the corridor
+sf_wimas_corridor <- 
+  st_filter(sf_wimas_trim, sf_corridor)
+
+# plot and inspect
+ggplot() +
+  geom_sf(data = sf_corridor) +
+  geom_sf(data = sf_wimas_corridor, color = col.cat.red)
+
+# calculate total water use by year, use, and source
+df_wimas_corridorTotal <- 
+  sf_wimas_corridor |> 
+  st_drop_geometry() |> 
+  group_by(year, use, source) |> 
+  summarise(
+    WaterUse_Total_m3 = sum(amt * 1233.4818375475, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# plot to inspect
+ggplot(df_wimas_corridorTotal, aes(x = year, y = WaterUse_Total_m3, fill = source)) +
+  geom_col(position = "stack") +
+  facet_wrap(~ use, scales = "free_y")
+
+# calculate total water use in each year by sector
+df_wimas_corridorSector <- 
+  df_wimas_corridorTotal |> 
+  group_by(year, use) |> 
+  summarise(
+    waterUse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# calculate groundwater use in each year by sector
+df_wimas_corridorSectorGW <- 
+  df_wimas_corridorTotal |> 
+  subset(source == "G") |>
+  group_by(year, use) |> 
+  summarise(
+    GWuse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# calculate surface water use in each year by sector
+df_wimas_corridorSectorSW <- 
+  df_wimas_corridorTotal |> 
+  subset(source == "S") |>
+  group_by(year, use) |> 
+  summarise(
+    SWuse_m3 = sum(WaterUse_Total_m3, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# join
+df_wimas_corridor_out <-
+  left_join(df_wimas_corridorSector, df_wimas_corridorSectorGW, by = c("year", "use")) |> 
+  left_join(df_wimas_corridorSectorSW, by = c("year", "use")) |>
+  rename(all_of(c("Year" = "year",
+                  "Sector" = "use")))
+
+write_csv(df_wimas_corridor_out, file.path("data", "WaterUseData_AlluvialCorridor.csv"))
+
+
+# Process for counties ----------------------------------------------------
+
+# in sf_wimas, subset to IRR and add a column for FIPS
+sf_wimas_counties <- 
+  sf_wimas_trim |> 
+  # trim to irrigation
+  subset(use == "IRR") |> 
+  st_join(sf_counties, join = st_within) |> 
+  # remove any points that don't fall within a county
+  filter(!is.na(FIPS)) |> 
+  # remove anything with NA or 0 for water use or irrigated acres
+  mutate(crop_code = as.numeric(crop_code)) |>
+  filter(is.finite(amt) & amt > 0 &
+           is.finite(acres_irr) & acres_irr > 0)
+
+ggplot() + 
+  geom_sf(data = sf_counties) +
+  geom_sf(data = sf_wimas_counties, aes(color = FIPS))
+
+# inspect crop codes
+sf_wimas_counties |> 
+  st_drop_geometry() |> 
+  group_by(crop_code) |> 
+  summarize(count = n()) |> 
+  arrange(-count)
+
+# frequent crops
+#  2 = corn
+#  4 = soybeans
+#  24 = corn & soybeans
+#  11 = golf course
+#  16 = more than one crop
+#  15 = other
+# other crops of interest
+#  3 = grain sorghum
+#  5 = wheat
+
+focus_crops <- c(2, 3, 4, 5)
+df_cropNames <- data.frame(
+  crop_code = c(2, 3, 4, 5),
+  Crop = c("Corn", "Grain Sorghum", "Soybeans", "Wheat")
+  )
+
+# calculate applied irrigation for each point
+df_wimas_counties_points <-
+  sf_wimas_counties |> 
+  st_drop_geometry() |> 
+  left_join(df_cropNames, by = "crop_code") |> 
+  # filter to focus crops
+  filter(crop_code %in% focus_crops) |> 
+  # irrigated area screening following Obembe et al (2023)
+  filter(acres_irr >= 40 & acres_irr <= 500) |> 
+  # unit conversions
+  mutate(irrigation_m3 = amt * 1233.4818375475,
+         cropArea_ha = acres_irr * 0.404686,
+         irrigation_mm = 1000 * irrigation_m3 / (cropArea_ha * 10000))
+
+# for each year, crop_code, and FIPS: calculate the mean, median, p25, p75, p5, and p95 irrigation_mm
+df_wimas_counties_out <- 
+  df_wimas_counties_points |> 
+  group_by(year, Crop, FIPS) |> 
+  summarise(
+    irrigation_mm_mean = mean(irrigation_mm, na.rm = TRUE),
+    irrigation_mm_median = median(irrigation_mm, na.rm = TRUE),
+    irrigation_mm_stdev = sd(irrigation_mm, na.rm = TRUE),
+    irrigation_mm_p5 = quantile(irrigation_mm, 0.05, na.rm = TRUE),
+    irrigation_mm_p25 = quantile(irrigation_mm, 0.25, na.rm = TRUE),
+    irrigation_mm_p75 = quantile(irrigation_mm, 0.75, na.rm = TRUE),
+    irrigation_mm_p95 = quantile(irrigation_mm, 0.95, na.rm = TRUE),
+    n_pdiv = n(),
+    .groups = "drop"
+  ) |> 
+  rename(Year = year)
+
+# plot to inspect
+ggplot(df_wimas_counties_points, aes(x = factor(crop_code), y = irrigation_mm)) +
+  geom_boxplot()
+
+# save output
+write_csv(df_wimas_counties_out, file.path("data", "WaterUseData_County.csv"))
+          
