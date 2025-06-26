81d98cc9a92396c4695149c7d8fdf5b462cd199b
Sam Zipper <samzipper@ku.edu>
2025-06-26 09:59:00 -0500

    Add fertilizer data look-up table

--- code/DataPrep_Fertilizer.R
+++ code/DataPrep_Fertilizer.R
@@ -5,6 +5,9 @@
 # Purpose: This script creates a look-up table of fertilizer use by Crop and WaterManagement.
 #  The data are compiled in the SAFEKAW google drive by Ikenna and Sam, so this 
 #  script just gets the data and cleans it up for use in optimization.
+#  At some point in the future, it could be improved:
+#   - varying fertilizer based on counties
+#   - vary fertilizer based on yield using assumed yield target approach
 # 
 # Output is a CSV file: FertilizerData_EKSRB.csv
 # | Crop | WaterManagement | FertilizerUse_kgHa |
@@ -17,4 +20,10 @@
 source(file.path("code", "paths+packages.R"))
 
 # load data
-df_in <- read_csv(file.path(path_data, "FertilizerData_EKSRB.csv"))
+df_in <- read_csv(file.path(path_data, "FertilizerDataFromIkenna", "FertilizerData_EKSRB.csv"))
+
+# select columns, rename, save
+df_in |>
+  dplyr::select(Crop, WaterManagement, FertilizerUse_kgHa_Final) |> 
+  rename(FertilizerUse_kgHa = FertilizerUse_kgHa_Final) |> 
+  write_csv(file.path("data", "FertilizerData_EKSRB.csv"))
